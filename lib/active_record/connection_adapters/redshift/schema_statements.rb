da37bc0032b9f73877e46848a1fd0f21516b6054
Victor Miao <vmiao1@berkeley.edu>
2020-06-30 15:49:16 -0700

    Update comparison to false result from column definitions.

--- lib/active_record/connection_adapters/redshift/schema_statements.rb
+++ lib/active_record/connection_adapters/redshift/schema_statements.rb
@@ -192,10 +192,9 @@ module ActiveRecord
             default_value = extract_value_from_default(default)
             type_metadata = fetch_type_metadata(column_name, type, oid, fmod)
             default_function = extract_default_function(default_value, default)
-            new_column(column_name, default_value, type_metadata, notnull == 'f', table_name, default_function)
 
             auto_increment = "true" if (default and default.include?("identity")) or (default_value and default_value.include?("identity"))
-            new_column(column_name, default_value, type_metadata, notnull == 'f', table_name, default_function, encoding, auto_increment)
+            new_column(column_name, default_value, type_metadata, notnull == false, table_name, default_function, encoding, auto_increment)
           end
         end
 
